a99e6502f21a211ee57cb9ac8611f71f724448e2
bununichita <bununichita@gmail.com>
2023-05-08 20:51:37 +0300

    second_commit

--- src/task-2/sort_procs.asm
+++ src/task-2/sort_procs.asm
@@ -8,6 +8,7 @@ endstruc
 
 section .text
     global sort_procs
+    extern printf
 
 sort_procs:
     ;; DO NOT MODIFY
@@ -18,9 +19,33 @@ sort_procs:
     mov eax, [ebp + 12]     ; length
     ;; DO NOT MODIFY
 
-    ;; Your code starts here
+    ; Your code starts here
+
+    ; movzx ax, [edx + proc.pid]
+    ; PRINTF32 `%d\n\x0`, eax
+    ; movzx eax, byte [edx + proc.prio]
+    ; PRINTF32 `%d\n\x0`, eax
+    ; movzx ax, [edx + proc.time]
+    ; PRINTF32 `%d\n\x0`, eax
 
 
+    mov ecx, eax
+    mov ebx, 0
+for_i:
+    PRINTF32 `%d \x0`, ebx
+    push ebx
+    imul ebx, ebx, 5
+    movzx ax, [edx + ebx + proc.pid]
+    PRINTF32 `%d    \x0`, eax
+    movzx eax, byte [edx + ebx + proc.prio]
+    PRINTF32 `%d    \x0`, eax
+    movzx ax, [edx + ebx + proc.time]
+    PRINTF32 `%d\n\x0`, eax
+    pop ebx
+
+    inc ebx
+    cmp ebx, ecx
+    jl for_i
 
     ;; Your code ends here
     
